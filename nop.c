db845db5d4d3f159aeee5b5fecb299730e63c1a4
Mwadali <wellingtonmwadali@gmail.com>
2023-05-24 04:13:19 -0700

    nop.c file

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop -  do nothing
+ * @custom_stack: it does not matter
+ * @line_count : for nothing
+ *
+ * Return: NOTHING
+ */
+void nop(stack_t **custom_stack, unsigned int line_count)
+{
+	(void) custom_stack;
+	(void) line_count;
+}
