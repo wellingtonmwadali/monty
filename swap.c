431e3070a097d8667caea606cce36c4195596306
Mwadali <wellingtonmwadali@gmail.com>
2023-05-24 04:00:12 -0700

    swap.c file

---
+++ swap.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monty.h"
+
+/**
+ * swap -  swaps data from top to previous pos
+ * @custom_stack: stack given by header in main file
+ * @line_count: the number of lines
+ *
+ * Return: void
+ */
+void swap(stack_t **custom_stack, unsigned int line_count)
+{
+	stack_t *tmp = NULL;
+	int tmp_n = 0;
+
+	if (!custom_stack || !*custom_stack || !((*custom_stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_count);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *custom_stack;
+	tmp_n = tmp->n;
+	tmp->n = tmp_n;
+
+	tmp->n = tmp->next->n;
+	tmp->next->n = tmp_n;
+}
